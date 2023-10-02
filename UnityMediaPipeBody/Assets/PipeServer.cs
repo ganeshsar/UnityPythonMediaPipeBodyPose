3548b6c0a8d6add814dd57ee34fc32c746752e82
Ganesh Saraswat <gsaraswa@ualberta.ca>
2023-10-02 08:08:17 -0600

    Fixed bug
    Technically since we flipped using cv2.flip, we need to re flip in unity (otherwise get incorrect results if query landmarks in unity).
    LEGACY BEHAVIOR: comment #define FLIP
    CORRECT BEHAVIOR: leave everything as is
    NO FLIP: comment cv2.flip() in python, comment FLIP here.

--- UnityMediaPipeBody/Assets/PipeServer.cs
+++ UnityMediaPipeBody/Assets/PipeServer.cs
@@ -27,41 +27,6 @@ public class PipeServer : MonoBehaviour
     NamedPipeServerStream server;
 
     const int LANDMARK_COUNT = 33;
-    public enum Landmark {
-        NOSE = 0,
-        LEFT_EYE_INNER = 1,
-        LEFT_EYE = 2,
-        LEFT_EYE_OUTER = 3,
-        RIGHT_EYE_INNER = 4,
-        RIGHT_EYE = 5,
-        RIGHT_EYE_OUTER = 6,
-        LEFT_EAR = 7,
-        RIGHT_EAR = 8,
-        MOUTH_LEFT = 9,
-        MOUTH_RIGHT = 10,
-        LEFT_SHOULDER = 11,
-        RIGHT_SHOULDER = 12,
-        LEFT_ELBOW = 13,
-        RIGHT_ELBOW = 14,
-        LEFT_WRIST = 15,
-        RIGHT_WRIST = 16,
-        LEFT_PINKY = 17,
-        RIGHT_PINKY = 18,
-        LEFT_INDEX = 19,
-        RIGHT_INDEX = 20,
-        LEFT_THUMB = 21,
-        RIGHT_THUMB = 22,
-        LEFT_HIP = 23,
-        RIGHT_HIP = 24,
-        LEFT_KNEE = 25,
-        RIGHT_KNEE = 26,
-        LEFT_ANKLE = 27,
-        RIGHT_ANKLE = 28,
-        LEFT_HEEL = 29,
-        RIGHT_HEEL = 30,
-        LEFT_FOOT_INDEX = 31,
-        RIGHT_FOOT_INDEX = 32
-    }
     const int LINES_COUNT = 11;
 
     public struct AccumulatedBuffer
